cf5350aaae9a16e010f4fcec778eb893cf5bd365
Ian Dahlstrom <ian.dahlstrom@gmail.com>
2024-05-31 10:43:01 -0400

    wip: Got training working, next sampling

--- learning/diffusion/ddpm.py
+++ learning/diffusion/ddpm.py
@@ -10,18 +10,62 @@ repository](https://github.com/hojonathanho/diffusion).
 
 # Standard Library Imports
 from argparse import ArgumentParser, Namespace
+from functools import partial
 from pathlib import Path
+from secrets import randbits
+from typing import Any, Callable, Iterator
 
 # External Imports
+import numpy as np
 import jax
 from jax import numpy as jnp
-from jaxtyping import Array
+from jaxtyping import Array, PRNGKeyArray
 from flax import linen as nn
+from flax.training import train_state
+import optax  # type: ignore
+from datasets import Dataset, load_dataset  # type: ignore
+from PIL.Image import Resampling
+from tensorboardX import SummaryWriter  # type: ignore
+import tqdm
 
 
 # -------------------------------------------------------------------------
 #   Dataset Functions
 # -------------------------------------------------------------------------
+
+
+def create_iterator_factory(
+    xs: Array,
+    ys: Array | None = None,
+    batch_size: int = 128,
+) -> Callable[[PRNGKeyArray], Iterator[tuple[Array, Array]]]:
+    n = xs.shape[0]
+    ys = ys if ys is not None else jnp.zeros((n,))
+
+    def iter_fn(rng) -> Iterator[tuple[Array, Array]]:
+        perm = jax.random.permutation(rng, n)
+
+        for j in range(0, n, batch_size):
+            yield xs[perm[j : j + batch_size]], ys[perm[j : j + batch_size]]
+
+    return iter_fn
+
+
+def auto_iterator(
+    factory: Callable[[PRNGKeyArray], Iterator[tuple[Array, Array]]],
+    rng: PRNGKeyArray,
+    n_batches: int,
+) -> Iterator[tuple[Array, Array]]:
+    idx = 0
+    stream = factory(jax.random.fold_in(rng, idx))
+    while idx < n_batches:
+        try:
+            batch = next(stream)
+            yield batch
+            idx += 1
+
+        except StopIteration:
+            stream = factory(jax.random.fold_in(rng, idx))
 
 
 # -------------------------------------------------------------------------
@@ -209,8 +253,117 @@ class Unet(nn.Module):
 
 
 # -------------------------------------------------------------------------
-#   Training Functions
+#   Diffusion Functions
 # -------------------------------------------------------------------------
+
+
+def create_betas(start: float, end: float, n: int, rule: str) -> Array:
+    match rule:
+        case "linear":
+            return jnp.linspace(start, end, n)
+
+        case _:
+            raise ValueError(f"invalid `{rule=}` provided")
+
+
+def q_sample(
+    x0: Array,
+    t: Array,
+    noise: Array,
+    *,
+    sqrt_alpha_bar: Array,
+    sqrt_one_minus_alpha_bar: Array,
+) -> Array:
+    bs, *dims = x0.shape
+    ones = (1,) * len(dims)
+    a = sqrt_alpha_bar[t].reshape(bs, *ones)
+    b = sqrt_one_minus_alpha_bar[t].reshape(bs, *ones)
+    return a * x0 + b * noise
+
+
+# -------------------------------------------------------------------------
+#   Training Functions and Utilities
+# -------------------------------------------------------------------------
+
+
+class TrainState(train_state.TrainState):
+    rng: PRNGKeyArray
+
+
+def train(
+    net: Unet,
+    optimizer: optax.GradientTransformation,
+    dataset: Dataset,
+    rng: PRNGKeyArray,
+    n_train_steps: int,
+    writer: SummaryWriter,
+    batch_size: int,
+    n_diffusion_steps: int,
+    betas: Array,
+    ema: float,
+):
+    kinit, kdata, kstate, rng = jax.random.split(rng, 4)
+    imshape = dataset["image"][0].shape
+    vars = net.init(kinit, jnp.empty((1, *imshape)), jnp.empty((1,), dtype=jnp.int32))
+    alpha_bar = jnp.cumprod(1 - betas)
+
+    sampler = partial(
+        q_sample,
+        sqrt_alpha_bar=jnp.sqrt(alpha_bar),
+        sqrt_one_minus_alpha_bar=jnp.sqrt(1 - alpha_bar),
+    )
+    iterator = auto_iterator(
+        factory=create_iterator_factory(dataset["image"], dataset["label"], batch_size),
+        rng=kdata,
+        n_batches=n_train_steps,
+    )
+    state = TrainState.create(
+        apply_fn=net.apply,
+        params=vars["params"],
+        tx=optimizer,
+        rng=kstate,
+    )
+
+    for step, (x0, y) in tqdm.tqdm(enumerate(iterator), total=n_train_steps):
+        kt, knoise, rng = jax.random.split(rng, 3)
+        t = jax.random.randint(kt, (x0.shape[0],), 1, n_diffusion_steps)
+        noise = jax.random.normal(knoise, x0.shape)
+        xt = sampler(x0, t, noise)
+
+        new_state, metrics = train_step(state=state, xt=xt, t=t, noise=noise)
+        state = new_state.replace(
+            params=jax.tree_util.tree_map(
+                lambda new, old: ema * new + (1 - ema) * old,
+                new_state.params,
+                state.params,
+            )
+        )
+        writer.add_scalars("training", metrics, step)
+
+
+@jax.jit
+def train_step(
+    state: TrainState,
+    xt: Array,
+    t: Array,
+    noise: Array,
+) -> tuple[TrainState, dict[str, Array]]:
+    rng = jax.random.fold_in(key=state.rng, data=state.step)
+    loss, grads = jax.value_and_grad(
+        fun=lambda params: optax.squared_error(
+            predictions=state.apply_fn(
+                {"params": params},
+                xt,
+                t,
+                train=True,
+                rngs={"dropout": rng},
+            ),
+            targets=noise,
+        ).mean()
+    )(state.params)
+    state = state.apply_gradients(grads=grads)
+    metrics = {"loss": loss}
+    return state, metrics
 
 
 # -------------------------------------------------------------------------
@@ -218,17 +371,13 @@ class Unet(nn.Module):
 # -------------------------------------------------------------------------
 
 
+def sample():
+    pass
+
+
 # -------------------------------------------------------------------------
 #   CLI
 # -------------------------------------------------------------------------
-
-
-def train():
-    pass
-
-
-def sample():
-    pass
 
 
 def main(argv: tuple[str, ...] | None = None):
@@ -241,7 +390,67 @@ def parse_cli_arguments(argv: tuple[str, ...] | None = None) -> Namespace:
     subparsers = parser.add_subparsers()
 
     trainer = subparsers.add_parser("train")
-    trainer.set_defaults(execute=lambda args: train())
+    trainer.set_defaults(
+        execute=lambda args: train(
+            net=args.model_factory(args),
+            optimizer=args.optimizer_factory(args),
+            dataset=args.dataset_factory(args),
+            rng=args.rng_factory(args),
+            n_train_steps=args.n_train_steps,
+            writer=args.writer_factory(args),
+            batch_size=args.batch_size,
+            n_diffusion_steps=args.n_diffusion_steps,
+            betas=args.beta_factory(args),
+            ema=args.ema,
+        ),
+        model_factory=lambda args: Unet(
+            init_channels=args.d_model,
+            blocks_per_multiplier=args.blocks_per_multiplier,
+            channel_multipliers=args.multipliers,
+            n_groups=args.n_groups,
+            dropout=args.dropout,
+        ),
+        dataset_factory=lambda args: load_dataset(
+            args.dataset,
+            trust_remote_code=True,
+            split="test",
+        )
+        .map(
+            function=partial(
+                process_image,
+                imkey="image" if args.dataset == "mnist" else "img",
+                multiple_of=args.reshape_to_multiple_of,
+            ),
+            remove_columns="image" if args.dataset == "mnist" else "img",
+        )
+        .with_format("numpy"),
+        optimizer_factory=lambda args: optax.chain(
+            optax.clip_by_global_norm(args.clip_grad_norm),
+            optax.adamw(
+                optax.schedules.warmup_cosine_decay_schedule(
+                    init_value=0.0,
+                    peak_value=args.lr,
+                    warmup_steps=args.n_warmup_steps,
+                    decay_steps=args.n_train_steps,
+                ),
+            ),
+        ),
+        writer_factory=lambda args: SummaryWriter(
+            logdir=create_experiment_path(
+                args.experiment_dir,
+                args.experiment_name or args.dataset,
+                args.experiment_version,
+            ),
+            flush_secs=30,
+        ),
+        rng_factory=lambda args: jax.random.key(args.seed),
+        beta_factory=lambda args: create_betas(
+            args.beta_start,
+            args.beta_end,
+            args.n_diffusion_steps,
+            args.beta_schedule,
+        ),
+    )
     model = trainer.add_argument_group("Model Hyperparameters")
     model.add_argument("--d-model", type=int, default=32)
     model.add_argument("--multipliers", type=int, nargs="+", default=[1, 2, 2, 2])
@@ -263,6 +472,7 @@ def parse_cli_arguments(argv: tuple[str, ...] | None = None) -> Namespace:
     training = trainer.add_argument_group("Training Hyperparameters")
     training.add_argument("--n-train-steps", type=int, default=100000)
     training.add_argument("--batch-size", type=int, default=128)
+    training.add_argument("--ema", type=float, default=0.9999)
 
     data = trainer.add_argument_group("Data Hyperparameters")
     data.add_argument("--dataset", choices=("mnist", "cifar10"), default="mnist")
@@ -273,10 +483,44 @@ def parse_cli_arguments(argv: tuple[str, ...] | None = None) -> Namespace:
     exp.add_argument("--experiment-name")
     exp.add_argument("--experiment-version")
 
+    sys = trainer.add_argument_group("System Arguments")
+    sys.add_argument("--seed", type=int, default=randbits(30))
+
     sampler = subparsers.add_parser("sample")
     sampler.set_defaults(execute=lambda args: sample())
 
     return parser.parse_args(argv)
+
+
+def create_experiment_path(root: Path, name: str, version: str | None = None) -> Path:
+    base = root / name
+    if version is not None:
+        path = base / version
+        assert not path.exists()
+        return path
+
+    found = list(base.glob("*"))
+    idx = 0
+    version = "version_{}"
+    while version.format(idx) in found:
+        idx += 1
+
+    return base / version.format(idx)
+
+
+def process_image(
+    ex: dict[str, Any],
+    *,
+    imkey: str,
+    multiple_of: int,
+) -> dict[str, Any]:
+    h, w = ex[imkey].size
+    c = len(ex[imkey].getbands())
+    h = ((h // multiple_of) + 1) * multiple_of
+    w = ((w // multiple_of) + 1) * multiple_of
+
+    image = ex[imkey].resize((h, w), Resampling.BILINEAR)
+    return {"image": np.asarray(image).reshape([h, w, c])}
 
 
 # -------------------------------------------------------------------------
@@ -284,13 +528,4 @@ def parse_cli_arguments(argv: tuple[str, ...] | None = None) -> Namespace:
 # -------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    # main()
-
-    net = Unet(64, 1)
-    rng = jax.random.key(69)
-    kx, kt, knet, rng = jax.random.split(rng, 4)
-    x = jax.random.normal(kx, [1, 32, 32, 1])
-    t = jax.random.randint(kt, (1,), 0, 100)
-    y, _ = net.init_with_output(knet, x, t)
-
-    assert y.shape == x.shape
+    main()
